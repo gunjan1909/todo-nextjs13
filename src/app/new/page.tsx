07e1264a27210b9465caf90d2d36feae64411149
gunjan1909 <bhanarkaranita@gmail.com>
2023-05-24 00:09:35 +0530

    .

---
+++ src/app/new/page.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <>
+      <header className="flex justify-between mb-4 items-center">
+        <h1 className="text-2xl">TodoApp</h1>
+      </header>
+      <form className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+        />
+        <div className="flex gap-1 justify-end">
+          <Link
+            href=".."
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
---
+++ src/db.ts
@@ -0,0 +1,16 @@
+//https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+//to avoid problem of creating many prisma client caused by hot reloading of nextjs
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
